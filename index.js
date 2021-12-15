f098e7b15ec0092f9e30dfef8cbe5cfd3803c7d9
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-12-15 14:54:40 +0700

    change runner

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import RegistrationController from "./Web/Http/RegistrationController"
 
 const web = new RegistrationController()
 
-let responseFromWeb = web.register({name: 'Budi', age: 20})
+let responseFromBackend = web.register({name: 'Rahmat', age: 20})
 
-console.log(responseFromWeb)
+console.log(responseFromBackend)
