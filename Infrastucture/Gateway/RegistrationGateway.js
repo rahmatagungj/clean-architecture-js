e103f27d551fb6d8b4baddcffffc0b97b1c9e1bf
Rahmat Agung Julians <rahmatagungj@gmail.com>
2021-12-15 15:02:16 +0700

    add return value

--- Infrastucture/Gateway/RegistrationGateway.js
+++ Infrastucture/Gateway/RegistrationGateway.js
@@ -3,6 +3,8 @@ let ALL_REGISTRATION = []
 export default class RegistrationGateway {
   addRegistration (person) {
     ALL_REGISTRATION.push(person)
+
+    return ALL_REGISTRATION
   }
   
   getRegistrations () {
