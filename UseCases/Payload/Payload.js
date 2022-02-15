ad07a3ca33aba225265e569cd5f9731820a48c79
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-02-15 14:04:47 +0700

    add validation for payload status and data

--- UseCases/Payload/Payload.js
+++ UseCases/Payload/Payload.js
@@ -1,5 +1,9 @@
 export default class Payload {
   constructor (status, data) {
+    if (!status) throw new Error('Payload Status is required')
+
+    if (!data) throw new Error('Payload Data is required')
+
     this.status = status
     this.data = data
   }
