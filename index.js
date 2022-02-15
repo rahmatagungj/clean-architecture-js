0ff975dfa06f037e37a58feafc49d6c5ffeabbf6
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-02-15 14:29:16 +0700

    add test case

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
+import { registration } from "./DB/registration"
 import RegistrationController from "./Web/Http/RegistrationController"
 
 const web = new RegistrationController()
 
 let responseFromBackend = web.register({name: 'Rahmat', age: 20})
 
-console.log(responseFromBackend)
+
+// test case
+if (responseFromBackend !== registration) {
+  throw new Error('Registration failed, data not same and not saved')
+} else {
+  console.log("Respond from backend :", responseFromBackend)
+  console.log("All data on registration DB :", registration)
+}
+
