8adf66f12665102e8b350d27fbfb972b4c9fee26
Rahmat Agung Julians <rahmatagungj@gmail.com>
2022-02-15 16:32:49 +0700

    create other case

--- index.js
+++ index.js
@@ -3,14 +3,29 @@ import RegistrationController from "./Web/Http/RegistrationController"
 
 const web = new RegistrationController()
 
+// case 1
+console.log("Request from frontend :", {name: 'Rahmat', age: 20});
+console.log("=========================");
 let responseFromBackend = web.register({name: 'Rahmat', age: 20})
 
-
-// test case
+// test case 1
 if (responseFromBackend !== registration) {
-  throw new Error('Registration failed, data not same and not saved')
+  throw new Error('Registration 1 failed, data not same and not saved')
 } else {
   console.log("Respond from backend :", responseFromBackend)
   console.log("All data on registration DB :", registration)
 }
 
+
+// case 2 
+console.log("\nRequest from frontend :", {name: 'Agung', age: 21});
+console.log("=========================");
+let responseFromBackendTwo = web.register({name: 'Agung', age: 20})
+
+// test case 2
+if (responseFromBackendTwo !== registration) {
+  throw new Error('Registration 2 failed, data not same and not saved')
+} else {
+  console.log("Respond from backend :", responseFromBackendTwo)
+  console.log("All data on registration DB :", registration)
+}
